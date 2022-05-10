c8596e06c85b971494b5599249f2d7173b073f1c
Hanna Khalipava <105045490+HannaKha@users.noreply.github.com>
2022-05-10 18:15:38 +0200

    Update ViewController.swift

--- HomeWork/ViewController.swift
+++ HomeWork/ViewController.swift
@@ -8,6 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let a: Int = 2
+    let b: Int = 9
+    let c: Int = 6
+    let d: Int = 8
+    let a2: Double = 0.5
+    let b2: Double = 0.7
+    let c2: Double = 0.9
+    let d2: Double = 0.2
+    var summ = "a" + "b" + "c" + "d" 
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,4 +30,3 @@ class ViewController: UIViewController {
 
 }
 
-view 
